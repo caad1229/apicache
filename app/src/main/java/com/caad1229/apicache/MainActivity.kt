d7825cd4b7d7ef1ed9926e9da9d02dea20343706
Satoru Sasaki <satoru.sasaki@mixi.co.jp>
2018-06-18 14:49:13 +0900

    enable databinding

--- app/src/main/java/com/caad1229/apicache/MainActivity.kt
+++ app/src/main/java/com/caad1229/apicache/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.caad1229.apicache
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.caad1229.apicache.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
