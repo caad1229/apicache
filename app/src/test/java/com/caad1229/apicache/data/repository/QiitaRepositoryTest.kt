19428ff9b0e005546cf5daeaf7ea7b1670d262c9
Satoru Sasaki <satoru.sasaki@mixi.co.jp>
2018-06-22 12:47:43 +0900

    add repository test

---
+++ app/src/test/java/com/caad1229/apicache/data/repository/QiitaRepositoryTest.kt
@@ -0,0 +1,37 @@
+package com.caad1229.apicache.data.repository
+
+import com.caad1229.apicache.data.datasource.QiitaRemoteDataSource
+import com.caad1229.apicache.presentation.entity.qiita.QiitaItem
+import io.reactivex.Single
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class QiitaRepositoryTest {
+
+    private lateinit var repository: QiitaRepository
+    private val userId: String = "user12345"
+
+    private lateinit var mockedRemoteDataSource: QiitaRemoteDataSource
+    private lateinit var mockedRemoteData: QiitaItem
+
+    @Before
+    fun setUp() {
+        mockedRemoteData = mock(QiitaItem::class.java)
+        mockedRemoteDataSource = mock(QiitaRemoteDataSource::class.java)
+        `when`(mockedRemoteDataSource.getUserItem(userId)).thenReturn(Single.just(listOf(mockedRemoteData)))
+
+        repository = QiitaRepository(mockedRemoteDataSource)
+    }
+
+    @Test
+    fun getUserItem_リモートから取得() {
+        val result = repository.getUserItems(userId).blockingGet()
+        assertEquals(mockedRemoteData, result.first())
+    }
+}
